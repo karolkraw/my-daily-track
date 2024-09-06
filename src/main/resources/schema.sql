e0d1b942554167206cd1bb144de7d0f8237c1021
Karol <karolkrawczyk8650@gmail.com>
2024-09-07 00:00:11 +0200

    update schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,15 @@
-CREATE TABLE IF NOT EXISTS streak_tracker (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS sections (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    streak_created DATE NOT NULL,
-    streak_length LONG NOT NULL
+    created DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS streak (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    start_date DATE NOT NULL,
+    days LONG NOT NULL,
+    section_id INT,
+    CONSTRAINT fk_section FOREIGN KEY (section_id)
+            REFERENCES sections (id)
 );
