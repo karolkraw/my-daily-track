18d000790dbf145161153cd59bce26e60b860235
Karol <karolkrawczyk8650@gmail.com>
2024-09-07 19:51:51 +0200

    section entity

--- src/main/java/org/example/section/Section.java
+++ src/main/java/org/example/section/Section.java
@@ -1,17 +1,24 @@
 package org.example.section;
 
 import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedDate;
+import lombok.Getter;
+import lombok.Setter;
+import org.example.streak.Streak;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+@Getter
+@Setter
 @Entity
-@Table
+@Table(name = "sections")
 public class Section  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
-    @CreatedDate
+    private String name;
     private LocalDateTime created;
+    @OneToMany(mappedBy = "section", cascade = CascadeType.ALL)
+    private List<Streak> streaks = new ArrayList<>();
 }
