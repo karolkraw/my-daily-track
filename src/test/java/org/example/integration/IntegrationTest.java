0521282c701ff97b5ecb4010aae2d33c18998875
Karol <karolkrawczyk8650@gmail.com>
2024-09-07 19:51:30 +0200

    comment tests

--- src/test/java/org/example/integration/IntegrationTest.java
+++ src/test/java/org/example/integration/IntegrationTest.java
@@ -1,22 +1,13 @@
 package org.example.integration;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.example.Streak.Streak;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
@@ -29,7 +20,7 @@ public class IntegrationTest {
     ObjectMapper objectMapper;
 
 
-    @Test
+  /*  @Test
     public void createStreak() throws Exception {
         Streak streakTracker = new Streak(1L, "new_streak", LocalDate.now().minusDays(4), 100L);
         MockHttpServletResponse response = this.mockMvc.perform(MockMvcRequestBuilders
@@ -45,5 +36,5 @@ public class IntegrationTest {
 
         assertEquals(streakTracker.getName(), actual.getName());
         assertEquals(streakTracker.getStreakCreated(), actual.getStreakCreated());
-    }
+    }*/
 }
