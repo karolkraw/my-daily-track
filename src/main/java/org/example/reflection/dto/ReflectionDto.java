66798577da2164f93693a815e956ef8ef7858f65
Karol <karolkrawczyk8650@gmail.com>
2024-09-08 13:28:42 +0200

    update dto to handle date

---
+++ src/main/java/org/example/reflection/dto/ReflectionDto.java
@@ -0,0 +1,15 @@
+package org.example.reflection.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReflectionDto {
+    String content;
+    String created;
+}
