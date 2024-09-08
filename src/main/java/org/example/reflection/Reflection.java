61baa8e030e235f563d2150ebdb71ce3d1e124fd
Karol <karolkrawczyk8650@gmail.com>
2024-09-08 13:28:04 +0200

    add reflection entity

---
+++ src/main/java/org/example/reflection/Reflection.java
@@ -0,0 +1,25 @@
+package org.example.reflection;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.section.Section;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Reflection {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String content;
+    LocalDate created;
+    @ManyToOne
+    @JoinColumn(name = "section_id")
+    private Section section;
+}
+
