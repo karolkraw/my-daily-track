7095d99043673d5634b55bd6c35bf7e816712500
Karol <karolkrawczyk8650@gmail.com>
2024-09-14 16:01:42 +0200

    schema fixes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS sections (
     created DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS streak (
+CREATE TABLE IF NOT EXISTS streaks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
     start_date DATE NOT NULL,
@@ -14,11 +14,30 @@ CREATE TABLE IF NOT EXISTS streak (
             REFERENCES sections (id)
 );
 
-CREATE TABLE IF NOT EXISTS reflection (
+CREATE TABLE IF NOT EXISTS reflections (
     id INT AUTO_INCREMENT PRIMARY KEY,
     content TEXT NOT NULL,
     created DATE NOT NULL UNIQUE,
     section_id INT,
     CONSTRAINT fk_section_reflection FOREIGN KEY (section_id)
             REFERENCES sections (id)
+);
+
+CREATE TABLE IF NOT EXISTS goals (
+    id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title varchar(255) NOT NULL,
+    description text NOT NULL,
+    completed_date date NOT NULL,
+    created_date date NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS subtasks (
+    id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    goal_id bigint NOT NULL,
+    title varchar(255) NOT NULL,
+    description text NOT NULL,
+    completed_date date NOT NULL,
+    created_date date NOT NULL,
+    CONSTRAINT fk_goal FOREIGN KEY (goal_id)
+            REFERENCES goals (id)
 );
