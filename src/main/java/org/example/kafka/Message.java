b5dd32592f97fefb3c3a9cce1421041d0984482b
Karol <karolkrawczyk8650@gmail.com>
2024-09-18 00:07:11 +0200

    comment out json property

--- src/main/java/org/example/kafka/Message.java
+++ src/main/java/org/example/kafka/Message.java
@@ -1,31 +1,13 @@
 package org.example.kafka;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
+@Data
 public class Message {
-    @JsonProperty("title")
+    //@JsonProperty("title")
     private String title;
 
-    @JsonProperty("description")
+    //@JsonProperty("description")
     private String description;
-
-    // Constructor, getters, and setters
-    public Message() {}
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
-
