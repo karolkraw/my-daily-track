625db21d0f5256f0baf2f09d021ca77bde308e8c
Karol <karolkrawczyk8650@gmail.com>
2024-09-18 00:09:14 +0200

    remove comments

--- src/main/java/org/example/kafka/KafkaConsumerConfig.java
+++ src/main/java/org/example/kafka/KafkaConsumerConfig.java
@@ -24,20 +24,16 @@ public class KafkaConsumerConfig {
     public ConsumerFactory<String, Message> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
 
-        // Kafka consumer basic configuration
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "history-service-group");
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        // Configuring ErrorHandlingDeserializer for key and value
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
 
-        // ErrorHandlingDeserializer configuration with delegates
         config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class.getName()); // Delegate for value deserialization
         config.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class.getName()); // Delegate for key deserialization
 
-        // JsonDeserializer specific configuration
         config.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
         config.put(JsonDeserializer.VALUE_DEFAULT_TYPE, Message.class.getName()); // Default type for deserialization
 
