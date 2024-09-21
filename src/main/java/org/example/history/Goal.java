0897ce982a176a1c936f758c647ece2958a5b104
Karol <karolkrawczyk8650@gmail.com>
2024-09-22 00:13:16 +0200

    add table annotation

--- src/main/java/org/example/history/Goal.java
+++ src/main/java/org/example/history/Goal.java
@@ -1,20 +1,27 @@
 package org.example.history;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import org.example.section.Section;
 
 import java.util.List;
 
 @Entity
+@Table(name = "goals")
 public class Goal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String title;
-    private String description;
-    private String completedDate;
-    private String createdDate;
-    private boolean isHistory = false;
+    @JsonIgnore
+    public Long id;
+    public String title;
+    public String description;
+    public String completedDate;
+    public String createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "section_id")
+    private Section section;
 
     @OneToMany(mappedBy = "goal")
-    private List<Subtask> subtasks;
+    public List<Subtask> subtasks;
 }
