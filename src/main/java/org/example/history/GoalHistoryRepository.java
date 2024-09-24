23e107fe228c79c4ec8e32b543958e6a7045255f
Karol <karolkrawczyk8650@gmail.com>
2024-09-24 17:46:18 +0200

    add findAllBySectionName

--- src/main/java/org/example/history/GoalHistoryRepository.java
+++ src/main/java/org/example/history/GoalHistoryRepository.java
@@ -1,5 +1,13 @@
 package org.example.history;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface GoalHistoryRepository extends JpaRepository<Goal, Long> {}
+import java.util.List;
+
+public interface GoalHistoryRepository extends JpaRepository<Goal, Long> {
+    @Query("SELECT g FROM Goal g JOIN FETCH g.subtasks JOIN g.section s WHERE s.name = :sectionName")
+    List<Goal> findAllBySectionName(@Param("sectionName") String sectionName);
+}
+
