964e9b417c83c71d517dd2b00950ccaa138d3ff8
Karol <karolkrawczyk8650@gmail.com>
2024-09-24 17:45:47 +0200

    move GoalMessage

---
+++ src/main/java/org/example/kafka/GoalMessage.java
@@ -0,0 +1,24 @@
+package org.example.kafka;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.history.Subtask;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GoalMessage {
+    //@JsonProperty("title")
+    private String title;
+
+    //@JsonProperty("description")
+    private String description;
+    private String createdDate;
+    private String completedDate;
+    private List<Subtask> subtasks;
+}
