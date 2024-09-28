ea3552bd157e7708c0ad8dff3bb105daf48aed0a
Karol <karolkrawczyk8650@gmail.com>
2024-09-28 23:42:53 +0200

    change mapping logic

--- src/main/java/org/example/history/dto/GoalMapper.java
+++ src/main/java/org/example/history/dto/GoalMapper.java
@@ -1,49 +1,79 @@
 package org.example.history.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.example.history.Goal;
 import org.example.history.Subtask;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class GoalMapper {
+import static org.example.utils.DateUtils.convertLocalDateToString;
+import static org.example.utils.DateUtils.convertStringToLocalDate;
 
-    public static GoalDto toDto(Goal goal) {
+public class GoalMapper {
+    public static Goal mapDtoToGoal(GoalDto goalDto) {
+        if (goalDto == null) {
+            return null;
+        }
+        Goal goal = new Goal();
+        goal.title = goalDto.getTitle();
+        goal.description = goalDto.getDescription();
+        goal.createdDate = convertStringToLocalDate(goalDto.getCreatedDate());
+        goal.completedDate = convertStringToLocalDate(goalDto.getCompletedDate());
+        goal.subtasks = goalDto.getSubtasks().stream().map(GoalMapper::dtoToSubtask).toList();
+        return goal;
+    }
+
+    public static GoalDto goalToDto(Goal goal) {
         if (goal == null) {
             return null;
         }
 
         List<SubtaskDto> subtaskDtos = goal.subtasks.stream()
-                .map(GoalMapper::toDto)
+                .map(GoalMapper::subtaskToDto)
                 .collect(Collectors.toList());
 
-        return new GoalDto(
-                goal.title,
-                goal.description,
-                goal.completedDate,
-                goal.createdDate,
-                subtaskDtos
-        );
+        GoalDto goalDto = new GoalDto();
+        goalDto.title = goal.title;
+        goalDto.description = goal.description;
+        goalDto.createdDate = convertLocalDateToString(goal.createdDate);
+        goalDto.completedDate = convertLocalDateToString(goal.completedDate);
+        goalDto.sectionName = goal.getSection().getName();
+        goalDto.subtasks = subtaskDtos;
+
+        return goalDto;
     }
 
-    public static SubtaskDto toDto(Subtask subtask) {
+    public static SubtaskDto subtaskToDto(Subtask subtask) {
         if (subtask == null) {
             return null;
         }
 
-        return new SubtaskDto(
-                subtask.title,
-                subtask.description,
-                subtask.completedDate,
-                subtask.createdDate
-        );
+        SubtaskDto subtaskDto = new SubtaskDto();
+        subtaskDto.title = subtask.title;
+        subtaskDto.description = subtask.description;
+        subtaskDto.createdDate = convertLocalDateToString(subtask.createdDate);
+        subtaskDto.completedDate = convertLocalDateToString(subtask.completedDate);
+
+        return subtaskDto;
     }
 
-    public static List<GoalDto> toDtoList(List<Goal> goals) {
+    public static Subtask dtoToSubtask(SubtaskDto subtaskDto) {
+        if (subtaskDto == null) {
+            return null;
+        }
+
+        Subtask subtask = new Subtask();
+        subtask.title = subtaskDto.title;
+        subtask.description = subtaskDto.description;
+        subtask.createdDate = convertStringToLocalDate(subtaskDto.createdDate);
+        subtask.completedDate = convertStringToLocalDate(subtaskDto.completedDate);
+
+        return subtask;
+    }
+
+    public static List<GoalDto> goalsToDtoList(List<Goal> goals) {
         return goals.stream()
-                .map(GoalMapper::toDto)
+                .map(GoalMapper::goalToDto)
                 .collect(Collectors.toList());
     }
 }
