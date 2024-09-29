2b233d9fca38742f42e96d0f49cddcc31de6360e
Karol <karolkrawczyk8650@gmail.com>
2024-09-29 09:59:42 +0200

    null check

--- src/main/java/org/example/utils/DateUtils.java
+++ src/main/java/org/example/utils/DateUtils.java
@@ -5,12 +5,15 @@ import java.time.format.DateTimeFormatter;
 
 public class DateUtils {
     public static LocalDate convertStringToLocalDate(String date) {
+        if (date == null) return null;
+
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         LocalDate.parse(date, formatter);
         return LocalDate.parse(date, formatter);
     }
 
     public static String convertLocalDateToString(LocalDate date) {
+        if (date == null) return null;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         return date.format(formatter);
     }
