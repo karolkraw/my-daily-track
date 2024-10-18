1880724ff2c0634de11f1a37cb5be7edcc0146c2
Karol <karolkrawczyk8650@gmail.com>
2024-10-18 09:56:17 +0200

    delete method

--- src/main/java/org/example/section/SectionService.java
+++ src/main/java/org/example/section/SectionService.java
@@ -1,6 +1,7 @@
 package org.example.section;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,5 +22,10 @@ public class SectionService {
 
     public Section getSectionByName(String sectionName) {
         return sectionRepository.findByName(sectionName);
+    }
+
+    @Transactional
+    void deleteSection(String name) {
+        sectionRepository.deleteByName(name);
     }
 }
