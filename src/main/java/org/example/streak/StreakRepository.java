4ba65f5fc61f01fdf6644da2cee8dc3cc75e6700
Karol <karolkrawczyk8650@gmail.com>
2024-10-18 11:41:41 +0200

    new repo methods

--- src/main/java/org/example/streak/StreakRepository.java
+++ src/main/java/org/example/streak/StreakRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface StreakRepository extends JpaRepository <Streak, Long> {
-    Streak findByName(String name);
+    Streak findByNameAndSection_Name(String name, String sectionName);
     List<Streak> findBySection_Name(String sectionName);
-    void deleteByName(String name);
+    void deleteByNameAndSection_Name(String name, String sectionName);
 }
