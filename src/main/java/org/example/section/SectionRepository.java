b758072c8de2b3f4f3efcbb4e6adfa31e533dcc6
Karol <karolkrawczyk8650@gmail.com>
2024-10-18 11:41:49 +0200

    new repo methods

--- src/main/java/org/example/section/SectionRepository.java
+++ src/main/java/org/example/section/SectionRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SectionRepository extends JpaRepository<Section, Long> {
     Section findByName(String sectionName);
+
+    void deleteByName(String name);
 }
