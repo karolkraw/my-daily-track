611289901675c7508c9412241413d6defc44378e
Karol <karolkrawczyk8650@gmail.com>
2024-10-18 11:41:53 +0200

    new repo methods

--- src/main/java/org/example/history/GoalHistoryRepository.java
+++ src/main/java/org/example/history/GoalHistoryRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface GoalHistoryRepository extends JpaRepository<Goal, Long> {
-    @Query("SELECT g FROM Goal g LEFT JOIN FETCH g.subtasks JOIN g.section s WHERE s.name = :sectionName ORDER BY g.completedDate")
+    @Query("SELECT g FROM Goal g LEFT JOIN FETCH g.subtasks JOIN g.section s WHERE s.name = :sectionName ORDER BY g.completedDate desc")
     List<Goal> findAllBySectionName(@Param("sectionName") String sectionName);
 }
 
